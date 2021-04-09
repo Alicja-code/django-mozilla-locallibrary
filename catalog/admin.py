e3a7fde48b46c21f828c1cf76166836d880f7e7e
Alicja <32157812+AlicjaHelenaM@users.noreply.github.com>
2021-04-09 12:06:38 +0200

    part 9 forms

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Author, Genre, Book, BookInstance, Language
 
+from .models import Author, Genre, Book, BookInstance, Language
 
 # Register your models here.
 
--- catalog/urls.py
+++ catalog/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path, re_path
+
 from . import views
 
 urlpatterns = [
@@ -13,4 +14,13 @@ urlpatterns = [
     path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
     # challenge: page only visible for librarians, that displays all books that have been borrowed, and which includes the name of each borrower
     path('borrowed/', views.LoanedBooksAllListView.as_view(), name='all-borrowed'),
+    # URL configuration for the renew-books page
+    path('book/<uuid:pk>/renew/', views.renew_book_librarian, name='renew-book-librarian'),
+    path('author/create/', views.AuthorCreate.as_view(), name='author-create'),
+    path('author/<int:pk>/update/', views.AuthorUpdate.as_view(), name='author-update'),
+    path('author/<int:pk>/delete/', views.AuthorDelete.as_view(), name='author-delete'),
+    # challenge
+    path('book/create/', views.BookCreate.as_view(), name='book-create'),
+    path('book/<int:pk>/update/', views.BookUpdate.as_view(), name='book-update'),
+    path('book/<int:pk>/delete/', views.BookDelete.as_view(), name='book-delete'),
 ]
